280f0e8ba9eab70f895e272f4333d016347f7ba5
OskarSui <oskon.suiutbekov@gmail.com>
2026-09-14 11:09:49 +0600

    fix: fixing the docker compose

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,13 +1,12 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
 
-export default defineConfig({
+export default {
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL,
   },
-});
+};
